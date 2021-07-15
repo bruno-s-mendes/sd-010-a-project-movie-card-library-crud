b901585eb3bc8ccf7a3d08837f6c3b5dd099a616
Bruno Mendes <mendessbruno@gmail.com>
2021-07-15 16:38:24 -0300

    Updates

---
+++ precache-manifest.eda1d157510e517b2530e974f78ea973.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1676d0c5858cd85be1435bc1c353bc",
+    "url": "/sd-010-a-project-movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "7c0a03e5bb0de6bfd25c",
+    "url": "/sd-010-a-project-movie-card-library-crud/static/css/main.026a6b0b.chunk.css"
+  },
+  {
+    "revision": "866f5de27f34dadb75dd",
+    "url": "/sd-010-a-project-movie-card-library-crud/static/js/2.bc63a950.chunk.js"
+  },
+  {
+    "revision": "7c0a03e5bb0de6bfd25c",
+    "url": "/sd-010-a-project-movie-card-library-crud/static/js/main.4300f2ef.chunk.js"
+  },
+  {
+    "revision": "1392a13028380f76597a",
+    "url": "/sd-010-a-project-movie-card-library-crud/static/js/runtime-main.d93514d2.js"
+  }
+]);
